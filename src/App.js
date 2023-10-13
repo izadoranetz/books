b927e25d580e9be6defe6916f84bb03fec9b5f8d
izadoranetz <izadora.ns@gmail.com>
2023-10-12 21:41:11 -0300

    adding books

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ export function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title: title }];
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
     setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
