f14b3cbd0061976b9f20086814dac88044722c87
izadoranetz <izadora.ns@gmail.com>
2023-10-12 21:41:04 -0300

    adding books

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BookCreate } from './components/BookCreate';
 
 export function App() {
   const [books, setBooks] = useState([]);
+
   const createBook = (title) => {
-    console.log('Need to add book with title:', title);
+    const updatedBooks = [...books, { id: 123, title: title }];
+    setBooks(updatedBooks);
   };
 
   return (
